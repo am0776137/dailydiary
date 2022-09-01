a45693f1fff8cd8f847828314cf48b6351e34064
am0776137 <am0776137@gmail.com>
2022-09-01 11:56:26 +0500

    post model creation

--- diary/views.py
+++ diary/views.py
@@ -3,31 +3,35 @@ import re
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-diary_data = [
-    {
-        'author':'Abdullah',
-        'title':'My First Day',
-        'content':'    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'date_posted':'August, 15, 2022'
-    },
-    {
-        'author':'Abdullah',
-        'title':'My Second Day',
-        'content':'    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'date_posted':'August, 16, 2022'
-    },
-]
+# diary_data = [
+#     {
+#         'author':'Abdullah',
+#         'title':'My First Day',
+#         'content':'    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+#         'date_posted':'August, 15, 2022'
+#     },
+#     {
+#         'author':'Abdullah',
+#         'title':'My Second Day',
+#         'content':'    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+#         'date_posted':'August, 16, 2022'
+#     },
+# ]
 
 def home(request):
     """
         This view renders and returns the homepage of the logged-in user
     """
     
+    # context = {
+    #     'posts':diary_data
+    # }
     context = {
-        'posts':diary_data
+        'posts': Post.objects.all()
     }
 
     return render(request, 'diary/home.html', context=context)
